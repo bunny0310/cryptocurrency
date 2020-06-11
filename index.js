d68d00cc5b29565277192b26756a8d0bb1f2e2be
Ishaan Khurana <khuranaishaan5@gmail.com>
2020-06-10 22:23:38 -0400

    heroku updates

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ if (isDevelopment) {
 let PEER_PORT;
 if(process.env.GENERATE_PEER_PORT === 'true')
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random()*1000);
-const PORT = PEER_PORT || DEFAULT_PORT;
+const PORT = process.env.PORT||PEER_PORT || DEFAULT_PORT;
 app.listen(PORT,()=>{
     console.log("listening at http://localhost:",PORT);
     if(PORT!==DEFAULT_PORT)
